5cfc8a52c71537570fa7ccefbac292bf720b9797
Goran Gligorin <goran.gligorin@dlabs.si>
2020-12-27 23:19:09 +0100

    feat: add solutions for 2020, day 21

---
+++ 2020/day-21/part-1.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const { intersection, difference } = require('lodash');
+
+const rawInput = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
+
+function main(input) {
+  const foods = input
+    .split('\n')
+    .filter((l) => !!l)
+    .map((food) => {
+      let [ingredients, allergenList] = food.split(' (contains ');
+
+      ingredients = ingredients.split(' ');
+
+      if (allergenList) {
+        allergenList = allergenList.replace(')', '').split(', ').sort();
+      }
+
+      const allergens = allergenList
+        ? allergenList.map((a) => ({
+            name: a,
+            ingredients,
+          }))
+        : undefined;
+
+      return { ingredients, allergens };
+    });
+
+  const ingredients = foods.flatMap((f) => f.ingredients).sort();
+
+  const allergens = foods
+    .flatMap((f) => f.allergens)
+    .sort((a, b) => (a.name < b.name ? -1 : 1));
+
+  let uniqueAllergens = allergens
+    .map((a) => a.name)
+    .filter((a, i, arr) => arr.indexOf(a) === i)
+    .map((ua) => {
+      const allgs = allergens.filter((a) => a.name === ua);
+
+      initialIngs = allgs.shift().ingredients;
+
+      const ings = allgs.reduce(
+        (ings, a) => intersection(ings, a.ingredients),
+        initialIngs,
+      );
+
+      return {
+        name: ua,
+        ingredients: ings,
+      };
+    });
+
+  do {
+    const algsWith1Ing = uniqueAllergens.filter(
+      (ua) => ua.ingredients.length === 1,
+    );
+
+    if (algsWith1Ing.length === uniqueAllergens.length) {
+      break;
+    }
+
+    uniqueAllergens = uniqueAllergens.map((ua) => {
+      if (algsWith1Ing.filter((a1) => a1.name === ua.name).length > 0) {
+        return ua;
+      }
+
+      algsWith1Ing.forEach((a1) => {
+        ua.ingredients = difference(ua.ingredients, a1.ingredients);
+      });
+
+      return ua;
+    });
+  } while (true);
+
+  const nonAllergenIngredients = difference(
+    ingredients,
+    uniqueAllergens.flatMap((ua) => ua.ingredients),
+  );
+
+  console.log({ count: nonAllergenIngredients.length });
+}
+
+main(rawInput);
