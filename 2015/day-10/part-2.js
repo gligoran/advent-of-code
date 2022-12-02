c250228af471dee8dd454a0495764f65c34e137d
Goran Gligorin <goran@gligorin.com>
2022-12-02 21:42:44 +0100

    feat: add solutions for 2015, day 10

---
+++ 2015/day-10/part-2.js
@@ -0,0 +1,26 @@
+const input = '3113322113';
+
+const repetitions = 50;
+
+const lookAndSay = (sequence) => {
+  let newSequence = '';
+
+  while (sequence.length) {
+    const digit = sequence[0];
+    const regex = new RegExp(`^[${digit}]+`);
+    const match = sequence.match(regex)[0];
+
+    newSequence += `${match.length}${digit}`;
+
+    sequence = sequence.replace(match, '');
+  }
+
+  return newSequence;
+};
+
+let currentSequence = input;
+for (let i = 0; i < repetitions; i += 1) {
+  currentSequence = lookAndSay(currentSequence);
+}
+
+console.log(currentSequence.length);
