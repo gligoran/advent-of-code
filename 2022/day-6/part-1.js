a9c88f73bd227097978e4a32ee03f90a4c3c551a
Goran Gligorin <gligoran@gmail.com>
2022-12-06 09:20:08 +0100

    feat: add solutions for 2022, day 6

---
+++ 2022/day-6/part-1.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const rawInput = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
+
+const uniqueCharsNeeded = 4;
+let marker = 0;
+for (let i = uniqueCharsNeeded - 1; i < rawInput.length; i += 1) {
+  const chars = new Array(uniqueCharsNeeded)
+    .fill(null)
+    .map((g, index) => rawInput.charAt(i - index));
+
+  const uniqChars = _.uniq(chars);
+
+  if (uniqChars.length === uniqueCharsNeeded) {
+    marker = i + 1;
+    break;
+  }
+}
+
+console.log(marker);
