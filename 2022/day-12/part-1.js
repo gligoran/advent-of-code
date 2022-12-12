470f138ff4d50723dea75fe30a6cb1c0a9db7427
Goran Gligorin <gligoran@gmail.com>
2022-12-12 10:48:28 +0100

    feat: add solutions for 2022, day 12

---
+++ 2022/day-12/part-1.js
@@ -0,0 +1,111 @@
+const fs = require('fs');
+
+const rawInput = fs.readFileSync('./input.txt', { encoding: 'utf-8' });
+
+let S;
+let E;
+
+function GridPoint(elevation, x, y, steps) {
+  this.x = x;
+  this.y = y;
+  this.elevation = elevation;
+  this.steps = steps;
+}
+
+const grid = rawInput
+  .split('\n')
+  .filter((l) => !!l)
+  .map((line, lineIndex) =>
+    line.split('').map((char, charIndex) => {
+      if (char === 'S') {
+        S = { x: charIndex, y: lineIndex };
+        return 0;
+      }
+
+      if (char === 'E') {
+        E = { x: charIndex, y: lineIndex };
+        return 'z'.charCodeAt(0) - 'a'.charCodeAt(0);
+      }
+      return char.charCodeAt(0) - 'a'.charCodeAt(0);
+    }),
+  );
+
+const rows = grid.length;
+const cols = grid[0].length;
+
+const search = () => {
+  let openPositions = [new GridPoint(grid[S.y][S.x], S.x, S.y, 0)];
+  const closedPositions = [];
+
+  while (openPositions.length > 0) {
+    openPositions.sort((a, b) => a.steps - b.steps);
+
+    const currentPosition = openPositions.shift();
+    closedPositions.push(currentPosition);
+
+    if (currentPosition.x === E.x && currentPosition.y === E.y) {
+      return currentPosition.steps;
+    }
+
+    let neighbors = [];
+    if (currentPosition.y < rows - 1) {
+      neighbors.push(
+        new GridPoint(
+          grid[currentPosition.y + 1][currentPosition.x],
+          currentPosition.x,
+          currentPosition.y + 1,
+          currentPosition.steps + 1,
+        ),
+      );
+    }
+
+    if (currentPosition.y > 0) {
+      neighbors.push(
+        new GridPoint(
+          grid[currentPosition.y - 1][currentPosition.x],
+          currentPosition.x,
+          currentPosition.y - 1,
+          currentPosition.steps + 1,
+        ),
+      );
+    }
+
+    if (currentPosition.x < cols - 1) {
+      neighbors.push(
+        new GridPoint(
+          grid[currentPosition.y][currentPosition.x + 1],
+          currentPosition.x + 1,
+          currentPosition.y,
+          currentPosition.steps + 1,
+        ),
+      );
+    }
+
+    if (currentPosition.x > 0) {
+      neighbors.push(
+        new GridPoint(
+          grid[currentPosition.y][currentPosition.x - 1],
+          currentPosition.x - 1,
+          currentPosition.y,
+          currentPosition.steps + 1,
+        ),
+      );
+    }
+
+    neighbors = neighbors.filter(
+      (n) =>
+        n.elevation <= currentPosition.elevation + 1 &&
+        closedPositions.findIndex((p) => p.x === n.x && p.y === n.y) < 0,
+    );
+
+    openPositions.push(...neighbors);
+    openPositions = openPositions.filter(
+      (op1) =>
+        closedPositions.findIndex((op2) => op1.x === op2.x && op1.y === op2.y) <
+        0,
+    );
+  }
+};
+
+const steps = search();
+console.log(steps);
